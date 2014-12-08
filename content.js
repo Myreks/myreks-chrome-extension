e790fffcaa101370f977b3eb5565fbe7ae6c4e3a
joaoricardo <joaoricardo@myreks.com>
2014-12-08 13:02:33 -0200

    Adicionada verificacao do snippet.js antes de injetar

--- content.js
+++ content.js
@@ -1,14 +1,25 @@
 var snippetUrl = "http://myreks.com/plugin/recommendit/widget/snippet.js";
 
+var haveMyreksScript = function () {
+    var scripts = document.querySelectorAll("script");
+    for (var i = 0; i < scripts.length; i++) {
+        if (/myreks.+snippet\.js$/.test(scripts[i]))
+            return true;
+    }
+    return false;
+};
+
 var injectMyreks = function () {
-    var _el = document.createElement("script");
-    _el.src = snippetUrl;
-    document.querySelector("head").appendChild(_el);
-    if (window.location.hash.indexOf("myreks?visible") == -1)
+    if (!haveMyreksScript()) {
+        var _el = document.createElement("script");
+        _el.src = snippetUrl;
+        document.querySelector("head").appendChild(_el);
+    }
+    if (!/myreks\?visible/.test(window.location.hash))
         window.location.hash += "#myreks?visible=true"
 };
 
 chrome.runtime.sendMessage({type: "status"}, function (response) {
-    if(response.status == "true")
+    if (response.status == "true")
         injectMyreks();
 });
